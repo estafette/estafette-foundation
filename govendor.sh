d0c44043b9419350ec2317070331e89ca325739e
Jorrit Salverda <jsalverda@travix.com>
2019-01-24 20:45:36 +0100

    add useful functions for initializing logging, serving metrics, handling graceful shutdown and watching files for changes

---
+++ govendor.sh
@@ -0,0 +1,4 @@
+govendor init
+govendor fetch github.com/rs/zerolog/...@v1.11.0
+govendor fetch github.com/fsnotify/fsnotify@v1.4.7
+govendor fetch github.com/prometheus/client_golang/prometheus/...@v0.9.2
