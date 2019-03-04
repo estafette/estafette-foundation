5e4aecd6b5beb2b1997171835c499f43496501c3
Jorrit Salverda <jsalverda@travix.com>
2019-03-04 16:45:31 +0100

    use go modules

--- govendor.sh
+++
@@ -1,4 +0,0 @@
-govendor init
-govendor fetch github.com/rs/zerolog/...@v1.11.0
-govendor fetch github.com/fsnotify/fsnotify@v1.4.7
-govendor fetch github.com/prometheus/client_golang/prometheus/...@v0.9.2
