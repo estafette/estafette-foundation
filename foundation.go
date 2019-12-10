2d24c550a79484ac502120085274e817eb907b6e
Jorrit Salverda <jsalverda@travix.com>
2019-12-10 09:49:52 +0100

    add StringArrayContains

--- foundation.go
+++ foundation.go
@@ -144,3 +144,13 @@ func FileExists(filename string) bool {
 	}
 	return !info.IsDir()
 }
+
+// StringArrayContains checks if an array contains a specific value
+func StringArrayContains(array []string, search string) bool {
+	for _, v := range array {
+		if v == search {
+			return true
+		}
+	}
+	return false
+}
