5b3c1938ec72a43d73e526ca437aec2cc2534d9d
Jorrit Salverda <jsalverda@travix.com>
2020-06-23 10:40:38 +0200

    add tests for Retry

---
+++ retry_test.go
@@ -0,0 +1,112 @@
+package foundation
+
+import (
+	"errors"
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+var (
+	ErrToRetry    = fmt.Errorf("This is an that should be retried")
+	ErrToNotRetry = fmt.Errorf("This is an that should NOT be retried")
+)
+
+func TestRetry(t *testing.T) {
+	t.Run("ReturnsNilIfFunctionSucceeds", func(t *testing.T) {
+
+		attempts := 0
+		retryableFunc := func() error {
+			attempts++
+			return nil
+		}
+
+		// act
+		err := Retry(retryableFunc)
+
+		assert.Nil(t, err)
+		assert.Equal(t, 1, attempts)
+	})
+
+	t.Run("ReturnsNilIfFunctionSucceedsBeforeExhaustingAttemps", func(t *testing.T) {
+
+		attempts := 0
+		retryableFunc := func() error {
+			attempts++
+			if attempts < 5 {
+				return ErrToRetry
+			}
+			return nil
+		}
+
+		// act
+		err := Retry(retryableFunc, Attempts(5), DelayMillisecond(10), Fixed())
+
+		assert.Nil(t, err)
+		assert.Equal(t, 5, attempts)
+	})
+
+	t.Run("ReturnsErrIfFunctionFailsEveryTime", func(t *testing.T) {
+
+		attempts := 0
+		retryableFunc := func() error {
+			attempts++
+			return ErrToRetry
+		}
+
+		// act
+		err := Retry(retryableFunc, Attempts(5), DelayMillisecond(10), Fixed())
+
+		assert.NotNil(t, err)
+		assert.Equal(t, 5, attempts)
+	})
+
+	t.Run("RetriesForCustomRetryableErrorFunc", func(t *testing.T) {
+
+		attempts := 0
+		retryableFunc := func() error {
+			attempts++
+			return ErrToRetry
+		}
+
+		isRetryableErrorCustomOption := func(c *RetryConfig) {
+			c.IsRetryableError = func(err error) bool {
+				if errors.Is(err, ErrToNotRetry) {
+					return false
+				}
+				return true
+			}
+		}
+
+		// act
+		err := Retry(retryableFunc, isRetryableErrorCustomOption, Attempts(5), DelayMillisecond(10), Fixed())
+
+		assert.NotNil(t, err)
+		assert.Equal(t, 5, attempts)
+	})
+
+	t.Run("DoesNotRetryForCustomRetryableErrorFuncReturningFalse", func(t *testing.T) {
+
+		attempts := 0
+		retryableFunc := func() error {
+			attempts++
+			return ErrToNotRetry
+		}
+
+		isRetryableErrorCustomOption := func(c *RetryConfig) {
+			c.IsRetryableError = func(err error) bool {
+				if errors.Is(err, ErrToNotRetry) {
+					return false
+				}
+				return true
+			}
+		}
+
+		// act
+		err := Retry(retryableFunc, isRetryableErrorCustomOption, Attempts(5), DelayMillisecond(10), Fixed())
+
+		assert.NotNil(t, err)
+		assert.Equal(t, 1, attempts)
+	})
+}
